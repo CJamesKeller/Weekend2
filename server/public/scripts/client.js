79e057e861ecddda6c5030a233f12a4b2eac6c51
Chris Keller <cjameskeller@gmail.com>
2017-03-18 17:30:10 -0500

    working on support for multiple operators

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,38 +1,153 @@
-var buttonData;
-var storeNumberData;
+var buttonNumber;
+var numberArray = [];
+var counterNumbers;
+var buttonOperator;
+var operatorArray = [];
+var counterOperators;
 var calculationObject = {};
 
 $(document).ready(function()
 {
-    $("button").on("click", checkButton());
+    $("button").on("click", buttonClicked());
 
 //end of DocReady
 });
 
-function checkButton()
+//checks for which buttons have been pressed
+function buttonClicked()
+{
+  if(typeof(calculationObject.secondNum) === "string")
+  {
+    finalCheckButton();
+  }
+  else if(typeof(calculationObject.operator) === "string")
+  {
+    thirdCheckButton();
+  }
+  else if(typeof(calculationObject.firstNum) === "string")
+  {
+    secondCheckButton();
+  }
+  else
+  {
+    firstCheckButton();
+  }
+}
+
+//this function ensures first button is a number
+function firstCheckButton()
+{
+  if(this.data === 'number')
+  {
+    buttonNumber = this.dataNumber;
+    storeNumberData = buttonNumber;
+    var counterNumbers = 0;
+  }
+  else
+  {
+    errorMessage();
+  }
+}
+
+function errorMessage()
+{
+  $("#result").text("ERROR");
+}
+
+//this function concatenates multi-digit numbers or adds operator
+function secondCheckButton()
 {
   switch(this.data)
   {
   case 'number':
-  buttonData = this.dataNumber;
-  storeNumberData += buttonData;
+  buttonNumber = this.dataNumber;
+  storeNumberData += buttonNumber;
+  break;
+  case 'operator':
+  counterNumbers++;
+  calculationObject.firstNum = storeNumberData;
+  buttonData = this.dataOperator;
+  calculationObject.operator = buttonData;
+  thirdCheckButton();
   break;
   case 'spec':
   buttonData = this.dataSpec;
-  break;
-  case 'operator':
-  buttonData = this.dataOperator;
+  specialCases();
   break;
   default:
-  buttonData = "Error!";
+  errorMessage();
   break;
   }
-  createCalcObject();
 }
 
-function createCalcObject()
+function specialCases()
 {
+  if(buttonData == "clear")
+  {
+    $("#result").empty();
+  }
+  else if (buttonData == "perform")
+  {
+    errorMessage();
+  }
+}
 
+function thirdCheckButton()
+{
+  switch(this.data)
+  {
+  case 'number':
+  buttonNumber = this.dataNumber;
+  storeSecondNumber += buttonNumber;
+  break;
+  case 'operator':
+  errorMessage();
+  break;
+  case 'spec':
+  buttonData = this.dataSpec;
+  specialCases();
+  break;
+  default:
+  errorMessage();
+  break;
+  }
+}
+
+function finalSpecialCases()
+{
+  if(buttonData == "clear")
+  {
+    $("#result").empty();
+  }
+  else if (buttonData == "perform")
+  {
+    calculationObject.firstNum = storeNumberData;
+    sendReq();
+  }
+}
+
+function finalCheckButton()
+{
+  switch(this.data)
+  {
+  case 'number':
+  buttonNumber = this.dataNumber;
+  storeSecondNumber += buttonNumber;
+  break;
+  case 'operator':
+  calculationObject.secondNum = storeSecondNumber;
+  buttonData = this.dataOperator;
+  calculationObject.operator = buttonData;
+  thirdCheckButton();
+  break;
+  case 'spec':
+  buttonData = this.dataSpec;
+  specialCases();
+  break;
+  default:
+  errorMessage();
+  break;
+  }
 }
 
 function sendReq()
@@ -41,7 +156,7 @@ function sendReq()
     {
     type: "POST",
     url: "/index.html",
-    data: buttonData,
+    data: calculationObject,
     success: function(response)
       {
       updateDom(response);
