a9c272b7e4a0799ec8781aa7d7bab75adc861f73
Chris Keller <cjameskeller@gmail.com>
2017-03-18 15:29:15 -0500

    looking back at old code to check app client comm process

--- server/app.js
+++ server/app.js
@@ -5,16 +5,26 @@ var port = 5000;
 
 //server performs calculations and sends to client for display
 
-app.use(express.static("server/public"));
+app.use(express.static("server/public",
+{
+  index: "views/index.html"
+}));
 
-app.get("/", function(req,res)
+// app.get("/products", function(req, res)
+// {
+//   res.send(products);
+// });
+//
+// app.post("/newProduct", function(req, res)
+// {
+//   var newProduct = req.body;
+//   products.push(newProduct);
+//   res.sendStatus(200);
+// });
+
+app.get("/*", function(req,res)
 {
 
-});
-
-app.get("/", function( req, res )
-{
-  res.sendFile(path.resolve("server/public/views/index.html"));
 });
 
 app.listen(port, function()
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,19 +1,81 @@
-$(document).ready(function(){
-    $("#submitButton").on("click", function(){
-      var name = $("#nameAdd").val();
-      var details = $("#detailsAdd").val();
+var buttonData;
 
-      $.ajax({
-        type: "GET",
-        url: "/kitty/" + name + "/" + details,
-        // "/data/scott/teacher"
-        success: function(responseFromServer){
-        }
-      });
-    });
+$(document).ready(function()
+{
+    $("button").on("click", checkButton());
+
+    sendReq();
 
 //end of DocReady
 });
+
+function checkButton(data)
+{
+  switch(this.data)
+  {
+  case 'spec':
+  buttonData = this.dataSpec;
+  break;
+  case 'number':
+  buttonData = this.dataNumber;
+  break;
+  case 'operator':
+  buttonData = this.dataOperator;
+  break;
+  default:
+  buttonData = "Error!";
+  break;
+  }
+}
+
+function sendReq()
+{
+  $.ajax(
+    {
+    type: "GET",
+    success: updateDom
+    });
+}
+
+// $("form").on("submit", function(e)
+// {
+//   e.preventDefault();
+//   var product = {};
+//   product.make = $("#make").val();
+//   product.model = $("#model").val();
+//   console.log(product);
+//   $.ajax({
+//     type: "POST",
+//     url: "/newProduct",
+//     data: product,
+//     success: function(res)
+//     {
+//       //It will send status 200 anyways
+//       console.log("Success!");
+//     }
+//   });
+//   refreshProducts();
+// });
+//
+// function refreshProducts()
+// {
+//   //Takes in server-side "products"
+//     $.ajax({
+//       type: "GET",
+//       url: "/products",
+//       success: function(response)
+//       {
+//         console.log(response);
+//         displayProducts(response);
+//       }
+//     });
+// }
+
+
+function updateDom(response)
+{
+  $("#result").append(response);
+}
 
 //send numbers and operation to server as object
 //{ x: 3, y: 4, type: Add }
