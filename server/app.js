e782e4b72669b34a3c8ae9feab8a53c05e92fbd1
Chris Keller <cjameskeller@gmail.com>
2017-03-18 19:43:01 -0500

    i think the calculation should work

--- server/app.js
+++ server/app.js
@@ -3,30 +3,56 @@ var app = express();
 var path = require("path");
 var port = 5000;
 
-//server performs calculations and sends to client for display
+var numberArray = [];
+var operatorArray = [];
+var calculatingArray = [];
+var output;
 
+//initial page
 app.use(express.static("server/public",
 {
   index: "views/index.html"
 }));
 
-// app.get("/products", function(req, res)
-// {
-//   res.send(products);
-// });
-//
-// app.post("/newProduct", function(req, res)
-// {
-//   var newProduct = req.body;
-//   products.push(newProduct);
-//   res.sendStatus(200);
-// });
-
-app.get("/*", function(req,res)
+//recieves post request and sends confirmation
+app.post("/*" , function(req, res)
 {
-
+  var calculationObject = req;
+  numberArray = calculationObject.numbers;
+  operatorArray = calculationObject.operators;
+  calculateResult();
+  res.sendStatus(200);
 });
 
+//creates single array of alternating numbers and operators
+function calculateResult()
+{
+  for(var i = 0; i < numberArray.length; i++)
+  {
+    numberArray[i].push(calculatingArray);
+    if(operatorArray[i] !== undefined)
+    {
+      operatorArray[i].push(calculatingArray);
+    }
+  }
+  arrayToCalc();
+}
+
+//actually evaluates the whole input
+function arrayToCalc()
+{
+  var numString = calculatingArray.toString();
+  numString.replace(",", "");
+  output = eval(numString);
+}
+
+//sends evaluated answer as get response
+app.get("/*", function(req,res)
+{
+  res.send(output);
+});
+
+//this just shows it's working
 app.listen(port, function()
 {
   console.log("Listening on port! : ", port);
